d911f3c3ce084aaefd0037d57818f71dc091c748
Whitney <whitney.purdum@gmail.com>
2017-05-24 15:54:39 -0400

    update app.js file

--- js/app.js
+++ js/app.js
@@ -1,12 +1,30 @@
-// var data = [4, 8, 15, 16, 23, 42];
-//
-// var x = d3.scale.linear()
-//     .domain([0, d3.max(data)])
-//     .range([0, 420]);
-//
-// d3.select(".chart")
-//   .selectAll("div")
-//     .data(data)
-//   .enter().append("div")
-//     .style("width", function(d) { return x(d) + "px"; })
-//     .text(function(d) { return d; });
+function visualizeOranges() {
+  // This data will determine the size of our "oranges" that we will display
+  var orangeData = [10, 30, 50, 100]
+
+  // Target the div ('canvas') where our data will be displayed. Set its height and width.
+  var canvas = d3.select(".orangeContainer")
+    .append("svg")
+    .attr("width", 768)
+    .attr("height", 1024)
+
+    // set a variable for your shapes or representations. This will apply to all circles within the SVG
+  var oranges = canvas.selectAll("circle")
+      // pull in the data from array above
+    .data(orangeData)
+    //  'Enter' says "Okay, let's use the data now to create the elements"
+    .enter()
+    .append("circle")
+    .attr("fill", "orange")
+      // Define x axis of a circle. Each circle needs to hav ea unique cx value so they aren't on top of each other
+    .attr("cx", function(d, i){
+      return d + (i * 100)
+    })
+    .attr("cy", function(d){
+      return d;
+    })
+    .attr("r", function (d){
+      return d;
+    })
+      // Define y axis of a circle
+}
